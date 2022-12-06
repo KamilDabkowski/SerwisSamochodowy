2197fea45d5514d180de4e4ba30c8fdd4a78671d
kdpanetpl <kamil_dabkowski1@wp.pl>
2022-12-06 18:19:31 +0100

    czesc

---
+++ SerwisSamochodowy/Model/Czesc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisSamochodowy.Model
+{
+    internal class Czesc
+    {
+        #region properties
+
+        public int IdCzesc { get; set; }
+        public int IdUsterka { get; set; }
+        public string Nazwa { get; set; }
+        public float Cena { get; set; }
+        public string Numer { get; set; }
+        public bool Zamowione { get; set; }
+        public bool Dostarczone { get; set; }
+        public bool Wymienione { get; set; }
+        public Usterka usterka { get; set; }
+
+        #endregion
+
+        #region methods
+
+        void ZamówCzęści()
+        {
+            throw new NotImplementedException();
+        }
+
+        void PrzyjmijDostarczonaCzesc()
+        {
+            throw new NotImplementedException();
+        }
+
+        void WymieńCzęść()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
