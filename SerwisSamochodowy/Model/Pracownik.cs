d5c522f4ede98f06d832a7322deca45d66facb7f
kdpanetpl <kamil_dabkowski1@wp.pl>
2022-12-06 18:04:32 +0100

    pracownik

---
+++ SerwisSamochodowy/Model/Pracownik.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisSamochodowy.Model
+{
+    internal class Pracownik
+    {
+        public int IdPracownik { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+    }
+}
