5b2bdc1c00a6fbcca0b9e22d4a3406021894d7df
kdpanetpl <kamil_dabkowski1@wp.pl>
2022-12-06 18:03:38 +0100

    samochod

---
+++ SerwisSamochodowy/Model/Samochod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisSamochodowy.Model
+{
+    internal class Samochod
+    {
+        public int IdSamochod { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Silnik { get; set; }
+        public int RokProdukcji { get; set; }
+        public string NumerRejestracyjny { get; set; }
+    }
+}
