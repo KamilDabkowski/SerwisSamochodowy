b066d98efee915428d4af2c8de4f1dfd7475b50e
kdpanetpl <kamil_dabkowski1@wp.pl>
2022-12-06 18:01:44 +0100

    klient

---
+++ SerwisSamochodowy/Model/Klient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisSamochodowy.Model
+{
+    internal class Klient
+    {
+        public int IdKlient { get; set; }
+        public string NumerTelefonu { get; set; }
+    }
+}
