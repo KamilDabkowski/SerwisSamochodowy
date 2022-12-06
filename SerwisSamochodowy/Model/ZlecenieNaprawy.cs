52dc8628b032377b857bef61e81e8db01fa70fd7
kdpanetpl <kamil_dabkowski1@wp.pl>
2022-12-06 18:30:10 +0100

    zlecenie naprawy

---
+++ SerwisSamochodowy/Model/ZlecenieNaprawy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisSamochodowy.Model
+{
+    internal class ZlecenieNaprawy
+    {
+        #region properties
+
+        public int IdZlecenie { get; set; }
+        public int IdKlient { get; set; }
+        public Klient Klient { get; set; }
+        public int IdSamochod { get; set; }
+        public Samochod Samochod { get; set; }
+        public DateTime DataPrzyjecia { get; set; }
+        public bool Naprawione { get; set; }
+        public bool ZgloszoneZakonczenieNapraw { get; set; }
+        public DateTime DataOdbioru { get; set; }
+        public bool Zaplacone { get; set; }
+        public bool Odebrane { get; set; }
+        public List<Usterka> Usterki { get; set; }
+
+        #endregion
+
+        #region ctor
+
+        public ZlecenieNaprawy(Klient klient, Samochod samochod, List<Usterka> usterki)
+        {
+            this.Klient = klient;
+            this.Samochod = samochod;
+            this.Usterki = usterki;
+        }
+
+        #endregion
+
+        #region methods
+
+        void ZglosZakonczenieNapraw()
+        {
+            throw new NotImplementedException();
+        }
+
+        void WydajSamochod()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
