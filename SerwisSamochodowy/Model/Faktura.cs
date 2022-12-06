8c1c0295734fe26573a01e0f958b8931432df11e
kdpanetpl <kamil_dabkowski1@wp.pl>
2022-12-06 18:38:02 +0100

    faktury

---
+++ SerwisSamochodowy/Model/Faktura.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisSamochodowy.Model
+{
+    internal class Faktura
+    {
+        #region fields
+
+        private ZlecenieNaprawy _zlecenieNaprawy;
+
+        #endregion
+
+        #region properties
+
+        public int IdFaktura { get; private set; }
+        public int IdZlecenieNaprawy { get; private set; }
+        public DateTime DataWystawienia { get; private set; }
+        public string Numer { get; private set; }
+
+        #endregion
+
+        #region ctor
+
+        public Faktura()
+        {
+
+        }
+
+        #endregion
+
+        #region methods
+
+        void WystawFakture(ZlecenieNaprawy zlecenieNaprawy)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Zaplac()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        #endregion
+
+    }
+}
